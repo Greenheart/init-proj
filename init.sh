ddcedaa6dabe12f4bbac4e768bfb6153171a1eb3
Samuel Plumppu <greenheartmusic@gmail.com>
2015-12-31 11:07:34 +0100

    Atom now uses a package that remember latest projects which wont require project directory paths to be changed manually in the script

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Simple script that automatically sets up the development environment for my current project
+# Small script that automatically sets up the development environment for my current project
 # saves about 15 seconds * opening projects 2 times / day * 4 days / week = valuable time! ;)
 
 go2cpd () {
@@ -11,12 +11,8 @@ go2cpd () {
 cur_proj_name="Blaze Penguinz"
 echo "Starting: $cur_proj_name!  -  ($PWD)"
 
-proj_dir1="/home/sam/Documents/github/force-push-arena-2/"  #main
-proj_dir2="/home/sam/Documents/github/force-push-arena/"    #extra
-
-
 # start atom and firefox in the background and hide their output from nuhup.out log-files and terminal
-nohup atom $proj_dir1 $proj_dir2 >/dev/null 2>&1 &
+nohup atom >/dev/null 2>&1 &
 nohup firefox localhost:3000 >/dev/null 2>&1 &
 
 # finally, go to project directory and launch app-processes
