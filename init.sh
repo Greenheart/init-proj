fe6838807f9bf21e7c626675e87c684c73eeb6fc
Samuel Plumppu <greenheartmusic@gmail.com>
2016-01-05 14:36:28 +0100

    Made it easier to change project
    * Now possible to change project by editing only one string.
    
    * Script now uses $HOME instead of a hard-coded path. This means that
    the script should work on other computers too and for other users too.

--- init.sh
+++ init.sh
@@ -3,13 +3,14 @@
 # Small script that automatically sets up the development environment for my current project
 # saves about 30 seconds * opening projects 2 times / day * 4 days / week = valuable time! ;)
 
+cur_proj_name="blaze-penguinz"
+
 go2cpd () {
   # go to current project directory
-  cd "/home/sam/Documents/github/blaze-penguinz"
+  cd "$HOME/Documents/github/$cur_proj_name"
 }
 
-cur_proj_name="Blaze Penguinz"
-echo "Starting: $cur_proj_name!  -  ($PWD)"
+echo "Starting $cur_proj_name!"
 
 # start atom and firefox in the background and hide their output from nuhup.out log-files and terminal
 nohup atom >/dev/null 2>&1 &
